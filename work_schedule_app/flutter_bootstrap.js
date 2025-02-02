0ed8bb06198c85828e001f6c591a84eb81841b8b
asserty308 <yannickassert@gmail.com>
2025-02-02 22:43:14 +0100

    updated work schedule app

--- work_schedule_app/flutter_bootstrap.js
+++ work_schedule_app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "474198387"
+    serviceWorkerVersion: "1612670914"
   }
 });
